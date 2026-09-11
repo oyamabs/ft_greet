d95adead79bb7744dc0cc8b1b0d1e1da39932d16
Talen400 <talengithub0@gmail.com>
2026-09-11 20:46:59 -0300

    fix: handle API errors

--- ft_greet_Linux.sh
+++ ft_greet_Linux.sh
@@ -11,8 +11,8 @@
 #                                                                              #
 # **************************************************************************** #
 
-APP_ID=
-APP_SECRET=
+APP_ID="YOUR TOKEN"
+APP_SECRET="YOUR TOKEN"
 
 function check_bin()
 {
@@ -27,20 +27,38 @@ function check_bin()
 check_bin curl
 check_bin jq
 
-TOKEN=$(curl -s -X POST --data "grant_type=client_credentials&client_id=$APP_ID&client_secret=$APP_SECRET" https://api.intra.42.fr/oauth/token | jq .access_token | sed s/\"//g)
-RAW_JSON=$(curl -s -H "Authorization: Bearer $TOKEN" https://api.intra.42.fr/v2/users/$USER)
-LOGIN=$(echo $RAW_JSON | jq .login | sed s/\"//g)
-CORREC_POINTS=$(echo $RAW_JSON | jq .correction_point)
-WALLET=$(echo $RAW_JSON | jq .wallet)
-LOCATION=$(echo $RAW_JSON | jq .location | sed s/\"//g)
-CAMPUS=$(echo $RAW_JSON | jq '.campus | .[] | .name' | sed s/\"//g)
-FINISHED_PROJ=$(echo $RAW_JSON | jq '.projects_users | length')
+TOKEN_RESP=$(curl -s -X POST --data "grant_type=client_credentials&client_id=$APP_ID&client_secret=$APP_SECRET" https://api.intra.42.fr/oauth/token)
+TOKEN=$(echo "$TOKEN_RESP" | jq -r '.access_token')
+if [[ -z "$TOKEN" || "$TOKEN" == "null" ]]; then
+	echo "Failed to get an access token from the 42 API: $(echo "$TOKEN_RESP" | jq -r '.error_description // .error // .message // "unknown error"')" >&2
+	exit 1
+fi
+
+RAW_HTTP=$(curl -s -w "HTTP_CODE:%{http_code}" -H "Authorization: Bearer $TOKEN" https://api.intra.42.fr/v2/users/$USER)
+HTTP_CODE=${RAW_HTTP##*HTTP_CODE:}
+RAW_JSON=${RAW_HTTP%HTTP_CODE:*}
+if ! echo "$RAW_JSON" | jq -e '.login' >/dev/null 2>&1; then
+	MSG=$(echo "$RAW_JSON" | jq -r '.error_description // .message // .error')
+	[[ -z "$MSG" || "$MSG" == "null" ]] && MSG="unexpected response (HTTP $HTTP_CODE)"
+	echo "42 API error: $MSG" >&2
+	exit 1
+fi
+
+LOGIN=$(echo "$RAW_JSON" | jq -r '.login')
+CORREC_POINTS=$(echo "$RAW_JSON" | jq '.correction_point')
+WALLET=$(echo "$RAW_JSON" | jq '.wallet')
+LOCATION=$(echo "$RAW_JSON" | jq -r '.location // "off campus"')
+CAMPUS=$(echo "$RAW_JSON" | jq -r '.campus[0].name // "unknown campus"')
+FINISHED_PROJ=$(echo "$RAW_JSON" | jq '.projects_users | length')
 LOGTIMES=$(curl -s -G -H "Authorization: Bearer $TOKEN" "https://api.intra.42.fr/v2/users/$USER/locations" --data-urlencode "range[begin_at]=$(date -u -I),$(date -u -I -d "+1 days")")
 TOTAL_SECONDS=$(echo "$LOGTIMES" | jq '
-  def parse_date: sub("\\.[0-9]+Z$"; "Z") | fromdateiso8601;
-  [ .[] | 
-    ((.end_at // (now | todate) | parse_date) - (.begin_at | parse_date))
-  ] | add // 0 | floor
+  if type != "array" then 0 else
+    def parse_date: sub("\\.[0-9]+Z$"; "Z") | fromdateiso8601;
+    [ .[]? |
+      ((.end_at? // (now | todate)) | parse_date) -
+      ((.begin_at? // (now | todate)) | parse_date)
+    ] | add // 0 | floor
+  end
   ')
 HOURS=$((TOTAL_SECONDS / 3600))
 MINUTES=$(( (TOTAL_SECONDS % 3600) / 60 ))
